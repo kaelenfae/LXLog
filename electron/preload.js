573e094c581d1b02ef0285277882c56e1a772691
kaelenfae <140033465+kaelenfae@users.noreply.github.com>
2026-02-01 21:01:30 -0500

    fixed vulnerability

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-    onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (event, ...args) => callback(...args)),
-    onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', (event, ...args) => callback(...args)),
-    onUpdateError: (callback) => ipcRenderer.on('update-error', (event, ...args) => callback(...args)),
-    restartApp: () => ipcRenderer.send('restart-app'),
-    getVersion: () => process.versions.electron // or app version if exposed
-});
